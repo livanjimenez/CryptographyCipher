665d67774d01053cef6aec5a761f7b51a1011629
Livan Jimenez <jimenez.livan14@gmail.com>
2019-01-30 22:53:29 -0500

    new files

---
+++ Caesar.java
@@ -0,0 +1,33 @@
+public class Caesar extends Cipher {
+    public Caesar (String s) {
+        super(s);
+    }
+
+    public String encode(String word) {
+        return code(word, Constants.ENCODE_SHIFT);
+    }
+
+    public String decode(String word) {
+        return code(word, Constants.DECODE_SHIFT);
+    }
+
+    public code(String word, int SHIFT) {
+        StringBuffer result = new StringBuffer();
+
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            ch = determineCharacter(ch, SHIFT);
+            result.append(ch);
+        }
+
+        return result.toString();
+    }
+
+    public char determineCharacter(char ch, final int shift) {
+        if (Character.isUpperCase(ch)) {
+            ch = (char)('A' + (ch - 'A' + shift) % Constants.WRAP_AROUND);
+        }
+
+        return ch;
+    }
+}
